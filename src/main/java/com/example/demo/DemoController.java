bf0844d84695a771d0a2983e6a5a892476689861
Try-001 <Try-001@qq.com>
2021-05-28 13:52:23 +0800

    add health check interface

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,12 +3,34 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello,xxx";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String printWelcome() {
+        return "welcome测1试";
     }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/health/check", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String healthcheck() {
+        return "healthcheck:ok";
+    }
+
+    @RequestMapping(value = "/checkpreload.htm", method = RequestMethod.GET)
+    @ResponseBody
+    public String printSuccess() {
+        return "succes1";
+    }
+
 
 }
